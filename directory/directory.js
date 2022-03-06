fa439ced33c0cbbc680783d62040c277dbaa91da
Candyraider7th <pal19028@byui.edu>
2022-03-05 20:21:13 -0700

    e

--- directory/directory.js
+++ directory/directory.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = 'https://github.com/Candyraider7th/wdd230/blob/main/directory/data.json';
 
 fetch(requestURL)
   .then(function (response) {
