196fb1ea9e80b94d986df1df3dd72a7e91da9a28
Candyraider7th <pal19028@byui.edu>
2022-03-07 03:58:28 -0700

    ui

--- directory/directory.js
+++ directory/directory.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://github.com/Candyraider7th/wdd230/blob/main/directory/data.json';
+const requestURL = 'https://raw.githubusercontent.com/Candyraider7th/wdd230/main/directory/data.json';
 
 fetch(requestURL)
   .then(function (response) {
@@ -7,34 +7,81 @@ fetch(requestURL)
   .then(function (jsonObject) {
     console.table(jsonObject);
     const businesses = jsonObject['businesses'];
-    businesses.forEach(displaybusinesses);// temporary checking for valid response and data parsing
+    businesses.forEach(displaybusinessescards);// temporary checking for valid response and data parsing
+    businesses.forEach(displaybusinesseslist);
   });
 
-function displaybusinesses(business) {  // Create elements to add to the document
+function displaybusinessescards(business) {  // Create elements to add to the document
   let card = document.createElement('section');
   let address = document.createElement('p');
   let phonenumber = document.createElement('p');
-  let website = document.createElement('p');
+  let website = document.createElement('a');
+  let linkText = document.createElement('p');
   let img = document.createElement('img');
   address.textContent = `${business.address}`;
   phonenumber.textContent = `${business.phonenumber}`;
   website.textContent = `${business.website}`;
+  website.href = `${business.website}`;
   img.setAttribute('src', business.imageurl);
-  img.setAttribute('alt', `${prophet.name} ${prophet.lastname} ${prophet.order}`);
   
   // Add/append the section(card) with the h2 element;
+  card.appendChild(img);
   card.appendChild(address);
   card.appendChild(phonenumber);
   card.appendChild(website);
-  card.appendChild(img);
+  
   // Add/append the existing HTML div with the cards class with the section(card)
   document.querySelector('div.cards').appendChild(card);
+  
 }
 
-/*To clear out cards
-let card = document.querySelectorAll('.cards')
-card.forEach((item)=>{item.remove();
-})*/
+function displaybusinesseslist(business) {  // Create elements to add to the document
+  let list = document.createElement('section');
+  let address = document.createElement('p');
+  let phonenumber = document.createElement('p');
+  let website = document.createElement('a');
+  let linkText = document.createElement('p');
+  let name = document.createElement('p');
+  name.textContent = `${business.name}`
+  address.textContent = `${business.address}`;
+  phonenumber.textContent = `${business.phonenumber}`;
+  website.textContent = `${business.website}`;
+  website.href = `${business.website}`;
+
+  
+  // Add/append the section(card) with the h2 element;
+  list.appendChild(name);
+  list.appendChild(address);
+  list.appendChild(phonenumber);
+  list.appendChild(website);
+  
+  // Add/append the existing HTML div with the cards class with the section(card)
+  document.querySelector('div.list').appendChild(list);
+    
+}
+
+function buttonlist() {
+    let list = document.getElementById("list_directory");
+    let cards = document.getElementById("cards_directory");
+    list.classList.remove("hide");
+    cards.classList.add("hide");
+    cards.classList.remove("cards");
+    
+}
+
+function buttoncards() {
+    let list = document.getElementById("list_directory");
+    let cards = document.getElementById("cards_directory");
+    list.classList.add("hide");
+    cards.classList.remove("hide");
+    cards.classList.add("cards");
+    }
+
+
+//To clear out cards
+//let card = document.querySelectorAll('.cards')
+//card.forEach((item)=>{item.remove();
+//})
 
 /*function changeStyles () {
 //change styles
